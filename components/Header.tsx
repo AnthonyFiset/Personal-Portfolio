ca79c72d36104206346c2a9a7f3212b41382b3ff
Anthony <tonyfise@my.yorku.ca>
2022-12-17 18:57:44 -0500

    Finished the header & added animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,17 +7,17 @@ type Props = {}
 function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-10 xl:items-center'>
-        <div className='flex flex-row items-center'>
+        <motion.div initial={{x: -500, opacity: 0, scale:0.5}} animate={{x: 0, opacity: 1, scale: 1}} transition={{duration: 1.5}} className='flex flex-row items-center'>
             {/* Social Icons */}
             <SocialIcon url="https://github.com/AnthonyFiset" fgColor='gray' bgColor='transparent' />
             <SocialIcon url="https://www.linkedin.com/in/anthony-fiset-43672270/" fgColor='gray' bgColor='transparent' />
             <SocialIcon url="https://www.youtube.com/channel/UCCBunP94d0smP3wE79bHiZQ" fgColor='gray' bgColor='transparent' />
-        </div>
+        </motion.div>
 
-        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <motion.div initial={{x: 500, opacity: 0, scale:0.5}} animate={{x: 0, opacity: 1, scale: 1}} transition={{duration: 1.5}}  className='flex flex-row items-center text-gray-300 cursor-pointer'>
             <SocialIcon className="cursor-pointer" network="email" fgColor='gray' bgColor='transparent' />
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
-        </div>
+        </motion.div>
     </header>
   )
 }
