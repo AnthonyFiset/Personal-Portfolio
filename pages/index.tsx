fb3eac956f81e2a48a81ea7cd771eab7d20da3c8
Anthony <tonyfise@my.yorku.ca>
2022-12-18 16:53:03 -0500

    Finished hero, Now working on the about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
+import About from '../components/About'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Anthony Fiset</title>
         <meta name="description" content="Anthony Fiset's Personal Portfolio" />
@@ -20,7 +22,14 @@ export default function Home() {
 
       {/* Hero */}
 
+      <section id="hero" className='snap-start'>
+        <Hero />
+      </section>
+
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
       {/* Experience */}
 
