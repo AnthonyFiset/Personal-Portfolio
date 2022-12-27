1616198ca675088ff0392a81ea7a0f0e99011277
AnthonyFiset <tonyfise@my.yorku.ca>
2022-12-27 06:59:38 -0500

    added the contact me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -51,6 +52,10 @@ export default function Home() {
         </section>
 
       {/* Contact */}
+      <section id='contact' className='snap-center'>
+        <ContactMe />
+        </section>
+
         </div>
 
   )
